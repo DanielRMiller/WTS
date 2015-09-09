f20e529f5613978e28be83f08776f3e3a0b2c850
Daniel Miller <d.miller1.work@gmail.com>
2015-09-09 14:20:41 -0600

    Fixed some bugs and removed some unnecessary files.

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
+using System.Runtime.InteropServices;
 using Excel = Microsoft.Office.Interop.Excel;
 using Word = Microsoft.Office.Interop.Word;
 
@@ -13,10 +14,9 @@ namespace WordToSaras
         {
             // Create the Document to store data.
             // NOTE: this will take a param that is the saveTo filename
-            ExcelDoc xlDoc = new ExcelDoc();
-            ParserHelper p = new ParserHelper();
-            // NOTE: args should be replaced by the command line parameter that is the test filename
-            p.parseFile(args, xlDoc);
+            ExcelWriter excelWriter = new ExcelWriter(args[1], args[2]);
+            ParserHelper p = new ParserHelper(args[0], excelWriter);
+            p.parseFile();
             // This is just to help me see when the program is finished
             Console.Write("Press any key: ");
             Console.ReadKey();
@@ -24,23 +24,42 @@ namespace WordToSaras
     }
     class ParserHelper
     {
+        ExcelWriter excelWriter;
         Excel.Application excel;
         Word.Application word;
-        public void parseFile(string[] args, ExcelDoc xlDoc)
+        Word.Document docs;
+        int MAX;
+
+        public ParserHelper(string filename, ExcelWriter newExcelWriter)
         {
             // Set up excel
-            excel = xlDoc.xlApp;
+            excelWriter = newExcelWriter;
+            excel = newExcelWriter.xlApp;
 
             // This file will later be passed in trough an arg
-            FileInfo file = new FileInfo(@"C:\Users\Daniel\Desktop\Bio461Orig\Bio461Exam1.docx");
+            FileInfo file = new FileInfo(@filename);
 
             // Open the document as readOnly
             word = new Word.Application();
+            word.Visible = true;
             object miss = Missing.Value;
             object path = file.FullName;
             object readOnly = true;
-            Word.Document docs = word.Documents.Open(ref path, ref miss, ref readOnly, ref miss, ref miss, ref miss, ref miss, ref miss, ref miss, ref miss, ref miss, ref miss, ref miss, ref miss, ref miss, ref miss);
-            int MAX = docs.Paragraphs.Count;
+            object isVisible = true;
+            docs = word.Documents.Open(ref path, ref miss, ref readOnly, ref miss, ref miss, ref miss, ref miss, ref miss, ref miss, ref miss, ref miss, isVisible, ref miss, ref miss, ref miss, ref miss);
+            MAX = docs.Paragraphs.Count;
+
+        }
+
+        ~ParserHelper()
+        {
+            // Make like a tree
+            docs.Close();
+            word.Quit();
+        }
+
+        public void parseFile()
+        {
 
             // paragraph we are currently on
             int p = 1;
@@ -83,7 +102,7 @@ namespace WordToSaras
                 }
                 tmp = tmp.Trim(charsToTrim);
                 if (tmp != null && tmp != "")
-                    xlDoc.writePassage(tmp);
+                    excelWriter.writePassage(tmp);
 
                 // Get the question
                 if (p < MAX && docs.Paragraphs[p].Range.ListFormat.ListValue != 0)
@@ -97,49 +116,86 @@ namespace WordToSaras
                     {
                         // This checks if it is a blank line (do not need to add them into questionText)
                         tmp = docs.Paragraphs[p].Range.Text.ToString();
-                        if (tmp != null && tmp != "\r")
+                        if (tmp == null || tmp == "\r") { }  // to skip empty lines
+                        else if (tmp.StartsWith("1. "))
+                        {
+                            // MULTIPLE RESPONSE ANSWERS
+                            for (; p < MAX && docs.Paragraphs[p].Range.ListFormat.ListValue == 0; p++)
+                            {
+                                tmp = docs.Paragraphs[p].Range.Text.ToString().Trim(charsToTrim);
+                                if (tmp != "")
+                                    rTxt.Add(tmp.Substring(3).Trim(charsToTrim));
+                            }
+                            p--;
+                        }
+                        else
                             qTxt += docs.Paragraphs[p].Range.Text.ToString().Trim(charsToTrim) + '\n';
                     }
                 }
+                qTxt = qTxt.Trim(charsToTrim);
                 // Get the answers
-                for (; p < MAX && docs.Paragraphs[p].Range.ListFormat.ListValue != 0; p++)
+                // MULTIPLE CHOICE ANSWERS
+                string cResponse = "";
+                if (rTxt.Count == 0)
                 {
                     char letter = 'A';
-                    tmp = docs.Paragraphs[p].Range.Text.ToString();
-                    if (tmp != "\r")
-                        rTxt.Add(docs.Paragraphs[p].Range.Text.ToString().Trim(charsToTrim));
-                    else
+                    for (; p < MAX && docs.Paragraphs[p].Range.ListFormat.ListValue != 0; p++)
                     {
-                        rTxt.Add(letter.ToString());
-                        ++letter;
+                        tmp = docs.Paragraphs[p].Range.Text.ToString();
+                        if (tmp != "\r" && tmp != "*\r")
+                        {
+                            if (tmp[0] == '*')
+                            {
+                                rTxt.Add(tmp.Substring(1).Trim(charsToTrim));
+                                cResponse = rTxt.Count.ToString();
+                            }
+                            else
+                                rTxt.Add(tmp.Trim(charsToTrim));
+                        }
+                        else
+                        {
+                            rTxt.Add(letter.ToString());
+                            if (tmp[0] == '*')
+                                cResponse = rTxt.Count.ToString();
+                            ++letter;
+                        }
                     }
+                    // put question into excel document
+                    excelWriter.writeMCQuestion(qTxt, rTxt, cResponse);
                 }
-                // put question into excel document
-                xlDoc.writeQuestion(qTxt.Trim(charsToTrim), new List<string>(rTxt));
-
+                else
+                {
+                    for (; p < MAX && docs.Paragraphs[p].Range.ListFormat.ListValue != 0; p++)
+                    {
+                        tmp = docs.Paragraphs[p].Range.Text.ToString();
+                        if (tmp[0] == '*')
+                            cResponse = tmp.Substring(1).Trim(charsToTrim);
+                    }
+                    // put MR question into excel document
+                    excelWriter.writeMRQuestion(qTxt, rTxt, cResponse);
+                }
                 // clear up the responses
                 rTxt.Clear();
             }
-
-            // Make like a tree
-            docs.Close();
-            word.Quit();
         }
     }
-    class ExcelDoc
+    class ExcelWriter
     {
         public Excel.Application xlApp;
         public Excel.Workbook wb;
+        public Excel.Workbooks wbs;
         public Excel.Worksheet ws;
         private int row;
         private int questionNumber;
+        private string saveFile;
+        private string exam;
 
-        public void writeQuestion(string questionText, List<string> responses)
+        public void writeMCQuestion(string questionText, List<string> responses, string cResponse)
         {
             // CODE
-            ws.Cells[row, 1].Value2 = "MultiChoiceQ Code #" + (questionNumber);
+            ws.Cells[row, 1].Value2 = exam + "Question" + questionNumber;
             // LABEL
-            ws.Cells[row, 2].Value2 = "MiltiChoiceQ Label #" + (questionNumber);
+            ws.Cells[row, 2].Value2 = exam + "Question" + questionNumber;
             // PARENT REF (QUESTION HEADER) NOTE: WE MAY NEED TO DO THIS LATER
             // NOTE: ALL headers are at least going to be related to question following
             if (ws.Cells[row - 1, 4].Value2 == "P")
@@ -157,7 +213,9 @@ namespace WordToSaras
             // NUM OF OPTIONS
             ws.Cells[row, 13].Value2 = responses.Count;
             // CORRECT OPTION
-            ws.Cells[row, 14].Value2 = 1;
+            if (cResponse == "")
+                cResponse = "1";
+            ws.Cells[row, 14].Value2 = cResponse;
             // ITEM TEXT
             // NOTE: WE NEED RICH TEXT, THIS IS CURRENTLY PLAIN TEXT
             ws.Cells[row, 15].Value2 = questionText;
@@ -173,19 +231,53 @@ namespace WordToSaras
             ++row;
             ++questionNumber;
         }
+        public void writeMRQuestion(string questionText, List<string> responses, string cResponse)
+        {
+            // CODE
+            ws.Cells[row, 1].Value2 = exam + "Question" + questionNumber;
+            // LABEL
+            ws.Cells[row, 2].Value2 = exam + "Question" + questionNumber;
+            // PARENT REF (QUESTION HEADER) NOTE: WE MAY NEED TO DO THIS LATER
+            // NOTE: ALL headers are at least going to be related to question following
+            if (ws.Cells[row - 1, 4].Value2 == "P")
+                ws.Cells[row, 3].Value2 = ws.Cells[row - 1, 1];
+            // ITEM TYPE
+            ws.Cells[row, 4].Value2 = "MR";
+            // ITEM CORRECT MARKS
+            ws.Cells[row, 6].Value2 = 1;
+            // ITEM WRONG MARKS
+            ws.Cells[row, 7].Value2 = 0;
+            // LANGUAGE
+            ws.Cells[row, 11].Value2 = "English";
+            // SHUFFLE
+            ws.Cells[row, 12].Value2 = "True";
+            // NUM OF OPTIONS
+            ws.Cells[row, 13].Value2 = responses.Count;
+            // CORRECT OPTION
+            ws.Cells[row, 14].Value2 = cResponse;
+            // ITEM TEXT
+            // NOTE: WE NEED RICH TEXT, THIS IS CURRENTLY PLAIN TEXT
+            ws.Cells[row, 15].Value2 = questionText;
+            // OPTIONS
+            int col = 18;
+            foreach (string option in responses)
+            {
+                ws.Cells[row, col].Value2 = option;
+                col += 3;
+            }
 
+            // Get variables ready for next question
+            ++row;
+            ++questionNumber;
+        }
         public void writePassage(string passageText)
         {
-            // Write each question
+            /* CURRENTLY NOT WORKING
             {
                 // CODE
                 ws.Cells[row, 1].Value2 = "Passage Code #" + (questionNumber);
-                // LABEL
-                ws.Cells[row, 2].Value2 = "Passage Label #" + (questionNumber);
                 // ITEM TYPE
                 ws.Cells[row, 4].Value2 = "P";
-                // LANGUAGE
-                ws.Cells[row, 11].Value2 = "English";
                 // ITEM TEXT
                 // NOTE: WE NEED RICH TEXT, THIS IS CURRENTLY PLAIN TEXT
                 ws.Cells[row, 15].Value2 = passageText;
@@ -193,10 +285,12 @@ namespace WordToSaras
                 // Get variables ready for next entry
                 ++row;
             }
+            */
         }
-
-        public ExcelDoc()
+        public ExcelWriter(string newSaveFile, string newExam)
         {
+            saveFile = newSaveFile;
+            exam = newExam;
             row = 2;
             questionNumber = 1;
             xlApp = new Excel.Application();
@@ -206,9 +300,9 @@ namespace WordToSaras
                 return;
             }
 
-            xlApp.Visible = false; // Turn this to true if you want to see the program in the foreground
-
-            wb = xlApp.Workbooks.Add(Excel.XlWBATemplate.xlWBATWorksheet);
+            xlApp.Visible = true; // Turn this to true if you want to see the program in the foreground
+            wbs = xlApp.Workbooks;
+            wb = wbs.Add(Excel.XlWBATemplate.xlWBATWorksheet);
             ws = (Excel.Worksheet)wb.Worksheets[1];
 
             if (ws == null)
@@ -244,8 +338,7 @@ namespace WordToSaras
                 ws.Cells[1, col + 2].Value2 = "Option" + o + "_Rationale";
             }
         }
-
-        ~ExcelDoc()
+        ~ExcelWriter()
         {
             // This should make the worksheet look pretty 
             ws.Columns.AutoFit();
@@ -253,9 +346,23 @@ namespace WordToSaras
 
             // Close the document
             xlApp.DisplayAlerts = false; // This prevents the overwrite message popping up every time
-            wb.SaveAs(@"C:\Users\Daniel\Desktop\Bio461Orig\Bio461Exam1Excel.xlsx", Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Excel.XlSaveAsAccessMode.xlNoChange, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing);
+            wb.SaveAs(@saveFile, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Excel.XlSaveAsAccessMode.xlNoChange, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing);
             wb.Close();
             xlApp.Quit();
+
+            // Manual disposal because of COM
+        while (Marshal.ReleaseComObject(xlApp) != 0) { }
+        while (Marshal.ReleaseComObject(wb) != 0) { }
+        while (Marshal.ReleaseComObject(wbs) != 0) { }
+        while (Marshal.ReleaseComObject(ws) != 0) { }
+
+        xlApp = null;
+        wb = null;
+        wbs = null;
+        ws = null;
+
+        GC.Collect();
+        GC.WaitForPendingFinalizers();
         }
     }
 }
